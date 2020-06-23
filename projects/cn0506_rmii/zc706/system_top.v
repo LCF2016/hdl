c8ff7f562982aa2fb2933948f1db71f5639e3ec3
Alexandru Ardelean <alexandru.ardelean@analog.com>
2020-06-23 09:26:33 +0300

    cn0506_rmii/zc706: add support for reset GPIO
    Sames as for Zed, this hooks up the reset GPIOs for the 2 PHYs on the
    CN0506 board, to be able to reset the chip, and reinit/stabilize the clock.
    
    Signed-off-by: Alexandru Ardelean <alexandru.ardelean@analog.com>

--- projects/cn0506_rmii/zc706/system_top.v
+++ projects/cn0506_rmii/zc706/system_top.v
@@ -144,6 +144,12 @@ module system_top (
 
   assign gpio_i[63:36] = gpio_o[63:36];
 
+  assign gpio_reset_a = gpio_o[37];
+  assign gpio_reset_b = gpio_o[36];
+
+  assign reset_a = sys_reset_a | gpio_reset_a;
+  assign reset_b = sys_reset_b | gpio_reset_b;
+
   assign gpio_i[35] = link_st_a;
   assign gpio_i[34] = link_st_b;
 
@@ -209,8 +215,8 @@ module system_top (
     .spi1_sdo_i (1'b0),
     .spi1_sdo_o(),
 
-    .reset_a (reset_a),
-    .reset_b (reset_b),
+    .reset_a (sys_reset_a),
+    .reset_b (sys_reset_b),
     .ref_clk_50_a (rmii_rx_ref_clk_a),
     .ref_clk_50_b (rmii_rx_ref_clk_b),
 
